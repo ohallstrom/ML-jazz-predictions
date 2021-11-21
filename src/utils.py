0f5db641ef7259e6913bc79e8b9ed3c1fbfb5b9a
Oskar Hallström <79756299+ohallstrom@users.noreply.github.com>
2021-11-21 19:27:47 +0100

    Adding all roots

--- src/utils.py
+++ src/utils.py
@@ -7,10 +7,24 @@ Python Version: 3.8
 '''
 import numpy as np
 
-root_mappings = { # TODO: add all roots, also sharp ones
+root_mappings = {
     "C": 0,
+    "C#": 1,
     "Db": 1,
-    "B": 11 
+    "D": 2,
+    "D#": 3,
+    "Eb": 3,
+    "E": 4,
+    "F": 5,
+    "F#": 6,
+    "Gb": 6,
+    "G": 7,
+    "G#": 8,
+    "Ab": 8,
+    "A": 9,
+    "A#": 10,
+    "Bb": 10,
+    "B": 11
 }
 
 quality_mappings = { # TODO: add potential other "qualities"
