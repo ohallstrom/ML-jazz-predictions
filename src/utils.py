25b987753b4b61e79a4a3965eb5453e6f3e1a450
ohallstrom <oskar.hallstrom@outlook.com>
2021-11-25 15:06:18 +0100

    added chord_to_big_hot

--- src/utils.py
+++ src/utils.py
@@ -44,15 +44,16 @@ quality_mappings = { # TODO: add potential other "qualities"
 step_mappings = { # TODO: add more?
     2: 2,
     4: 5,
-    6: 9,
+    6: 9, # are there any flat or sharp cases? handle these in this case
     7: 10,
-    9: 2
+    # 9: 2 or handle flat and sharp cases
 }
 
 def chord_to_hot(chord):
     '''
     Projects a chord into a
-    multi-hot-encoded representation
+    multi-hot-encoded representation corresponding
+    to a midi-representation
     :param chord: String representation of chord
     :return: the chord in multi-hot-encoding
     ''' 
@@ -93,6 +94,62 @@ def chord_to_hot(chord):
     for num in chord_steps:
         if num in step_mappings:
             idx = chord_root + step_mappings[num]
+            if num == 7:
+                if chord_quality == "maj" or chord_quality == "j" : 
+                    idx += 1
+                elif chord_quality == "dim" or chord_quality =='o':
+                    idx -= 1
+            chord_multi_hot[idx % 12] = 1
+
+    return chord_multi_hot
+
+
+def chord_to_big_hot(chord):
+    '''
+    Projects a chord into a
+    multi-hot-encoded representation of length 24
+    :param chord: String representation of chord
+    :return: the chord in multi-hot-encoding,
+    first 12 positions corresponds to the chord format (without root),
+    and the 12 last positions to the root position
+    ''' 
+    # separate the root from the rest of the string
+    if len(chord) > 1:
+        if chord == 'NC':
+            return np.zeros(24)
+        elif chord[1] == 'b' or chord[1] == '#': # TODO: add character used for #
+            chord_root = root_mappings[chord[:2]]
+            chord_rest = chord[2:]
+        else:
+            chord_root = root_mappings[chord[0]]
+            chord_rest = chord[1:]
+    else:
+        chord_root = root_mappings[chord[0]]
+        chord_rest = ""
+
+    # retrieve chord quality and steps
+    chord_quality = ""
+    chord_steps = []
+
+    for ch in chord_rest:
+        if ch.isalpha() or ch == '+' or ch == '-':  # TODO : change to include other characters
+            chord_quality += ch
+        elif ch.isdigit():
+            chord_steps.append(int(ch))
+        elif ch == '/':
+            break
+    
+    # create multi-hot-encoding according to root, quality and steps
+    chord_multi_hot = np.zeros(24)
+    # add root pitch to second part of encoding
+    chord_multi_hot[chord_root+12] = 1
+    # add pitches according to chord quality
+    if chord_quality in quality_mappings.keys():
+        chord_multi_hot[quality_mappings[chord_quality] % 12] = 1
+    # add pitches according to extra numbers
+    for num in chord_steps:
+        if num in step_mappings:
+            idx = step_mappings[num]
             if num == 7:
                 if chord_quality == "maj" or chord_quality == "j" : 
                     idx += 1
